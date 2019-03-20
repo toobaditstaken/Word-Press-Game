0cf515324ab552ebd63fb2b81664d90cffaa9e67
toobaditstaken <alexabbott10@yahoo.com>
2019-03-19 19:50:41 -0500

    making javascript code so that the line fills in for when the correct letter is selected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,33 @@ var wins = 0;
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
 var letterChosenText = document.getElementById("letterschosen-text");
-var guessCountText = document.getElementById("guess-count")
+var guessCountText = document.getElementById("guess-count");
+var wordText = document.getElementById("word-text");
 var winsText = document.getElementById("wins-text");
 var arrayPossible =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var arraySelector =[];
 var guessCount = 10;
-var wordChoice = ["T","E","X","A","S"]
+var wordChoice = ["T","E","X","A","S"];
+var displayWord = ["_","_","_","_","_"];
 //var wordText = document.getElementById("word-text")
 
-
-
-
-
+function words(userInput) {
+   var isFound = false;
+    for (var i = 0; i < wordChoice; i++) {
+        if(wordChoice[i].toUpperCase() === userInput) {
+            displayWord[i] = userInput;
+            isFound = true;
+        }
+    }
+    return isFound;
+}
 
 
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
     var userInput = event.key.toUpperCase() ;
+    var isInWord = false;
     var isFound = false;
     
         for ( var i = 0; i < arrayPossible.length; i++) {
@@ -35,7 +44,9 @@ document.onkeyup = function(event) {
         }
         if 
         (isFound){
-        guessCount--;
+        isInWord = words(userInput);
+        if (isInWord === false){
+        guessCount--;}
         arraySelector.push(userInput);
         arraySelector.sort;
         break;
@@ -46,6 +57,7 @@ document.onkeyup = function(event) {
 
     userChoiceText.textContent = "You chose: " + userGuess;
 
+    wordText.textContent = "Word: " + displayWord;
     
     letterChosenText.textContent = "Already Guessed: " + arraySelector; 
     
